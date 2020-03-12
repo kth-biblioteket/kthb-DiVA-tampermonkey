606759a1e7d59a769166ff7944124c29cbbbd70a
thompa21 <35116728+thompa21@users.noreply.github.com>
2020-03-12 21:56:26 +0100

    Update DiVA_import.js

--- DiVA_import.js
+++ DiVA_import.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     DiVA_import
-// @version      1.0.4
+// @version      1.0.5
 // @author Thomas Lind
 // @updateURL    https://github.com/kth-biblioteket/kthb-DiVA-tampermonkey/raw/master/DiVA_import.js
 // @downloadURL  https://github.com/kth-biblioteket/kthb-DiVA-tampermonkey/raw/master/DiVA_import.js
@@ -83,7 +83,7 @@ function actionFunction() {
     $iframe.ready(function() {
         $iframe.contents().find("body").html(QC);
     });
-    callapi();
+    //callapi();
 }
 
 //Skapa QC + dagens datum
