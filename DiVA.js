c9ee9ecc9303cbd61ad5c894efb966294e071b15
Thomas Lind <thomas.lind@tlcgolfit.se>
2020-03-27 16:16:29 +0100

    13

--- DiVA.js
+++ DiVA.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name     DiVA
-// @version      1.0.12
+// @version      1.0.13
 // @description  En Apa för att hjälpa till med DiVA-arbetet på KTH Biblioteket
 // @author Thomas Lind
 // @updateURL    https://github.com/kth-biblioteket/kthb-DiVA-tampermonkey/raw/master/DiVA.js
